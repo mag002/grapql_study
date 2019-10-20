43fa9632023e11bfe0fa23eb09d6603d358cf356
mag002 <lehongphuc7394@gmail.com>
2019-10-20 20:19:40 +0700

    update

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -10,9 +10,12 @@ const {
 
 // dummy data
 var books = [
-  { name: "Name of the Shark", genre: "History", id: "1" },
-  { name: "The Last Shark", genre: "Action", id: "2" },
-  { name: "Beauty and the Shark", genre: "Cartoon", id: "3" }
+  { name: "Name of the Shark", genre: "History", id: "1", authorId: "1" },
+  { name: "The Last Shark", genre: "Action", id: "2", authorId: "3" },
+  { name: "Beauty and the Shark", genre: "Cartoon", id: "3", authorId: "2" },
+  { name: "Name of the Stork", genre: "History", id: "1", authorId: "1" },
+  { name: "The Last Stork", genre: "Action", id: "2", authorId: "3" },
+  { name: "Beauty and the Stork", genre: "Cartoon", id: "3", authorId: "2" }
 ];
 
 var authors = [
@@ -26,7 +29,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    genre: { type: GraphQLString }
+    genre: { type: GraphQLString },
+    author: {
+      type: AuthorType,
+      resolve(parent, args) {
+        return _.find(authors, { id: parent.authorId });
+      }
+    }
   })
 });
 
@@ -35,7 +44,13 @@ const AuthorType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLString },
     name: { type: GraphQLString },
-    age: { type: GraphQLInt }
+    age: { type: GraphQLInt },
+    books: {
+      type: BookType,
+      resolve(parent, args) {
+        return _.find();
+      }
+    }
   })
 });
 
